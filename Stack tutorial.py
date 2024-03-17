0ccee12df41cd0d1620a557db3a3e2910c120e20
Michael <mmarusinski123@wp.pl>
2024-03-18 00:51:15 +0100

    exercise 2 functions

--- Stack tutorial.py
+++ Stack tutorial.py
@@ -33,5 +33,36 @@ class Stack:
 stack = Stack()
 text = "We will conquere COVID-19"
 print(stack.reverse_string(text))
+   
+#Ex. 2
+
+def is_match(ch1, ch2):
+    match_dict = {
+        ')': '(',
+        ']': '[',
+        '}': '{'
+    }
+    return match_dict[ch1] == ch2
+
+def is_balanced(text):
+    stack = Stack()
+    for ch in text:
+        if ch=='(' or ch=='{' or ch == '[':
+            stack.push(ch)
+        if ch==')' or ch=='}' or ch == ']':
+            if stack.size() == 0:
+                return False
+            if not is_match(ch,stack.pop()):
+                return False
+            
+    return stack.size() == 0
+
+
+print(is_balanced("({a+b})"))
+print(is_balanced("))((a+b}{"))
+print(is_balanced("((a+b))"))
+print(is_balanced("((a+g))"))
+print(is_balanced("))"))
+print(is_balanced("[a+b]*(x+2y)*{gg+kk}"))
     
     
