8899b3ecb0276112c0808a9fd17ab19e5391ea27
Michael <mmarusinski123@wp.pl>
2024-03-19 16:17:10 +0100

    adding a graph class

---
+++ Graph tutorial.py
@@ -0,0 +1,45 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.children = []
+        self.parent = None
+    
+    def add_child(self, child):
+        child.parent = self
+        self.children.append(child)
+        
+    def print_tree(self):
+        print(self.data)
+        if self.children:
+            for child in self.children:
+                child.print_tree()
+                
+
+
+def build_product_tree():
+    root = Node("Electronics")
+
+    laptop = Node("Laptop")
+    laptop.add_child(Node("Mac"))
+    laptop.add_child(Node("Surface"))
+    laptop.add_child(Node("Thinkpad"))
+
+    cellphone = Node("Cell Phone")
+    cellphone.add_child(Node("iPhone"))
+    cellphone.add_child(Node("Google Pixel"))
+    cellphone.add_child(Node("Vivo"))
+
+    tv = Node("TV")
+    tv.add_child(Node("Samsung"))
+    tv.add_child(Node("LG"))
+
+    root.add_child(laptop)
+    root.add_child(cellphone)
+    root.add_child(tv)
+
+    root.print_tree()
+
+if __name__ == '__main__':
+    build_product_tree()
+    
+        
